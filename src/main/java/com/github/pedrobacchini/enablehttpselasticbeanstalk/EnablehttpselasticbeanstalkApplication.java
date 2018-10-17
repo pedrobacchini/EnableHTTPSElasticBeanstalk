d2cfa220b57d1a011f713d999603d0803f2eeb4c
Pedro Bacchini <pedrobacchini@outlook.com>
2018-10-17 10:52:44 -0300

    Primeiro Commit

---
+++ src/main/java/com/github/pedrobacchini/enablehttpselasticbeanstalk/EnablehttpselasticbeanstalkApplication.java
@@ -0,0 +1,12 @@
+package com.github.pedrobacchini.enablehttpselasticbeanstalk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnablehttpselasticbeanstalkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnablehttpselasticbeanstalkApplication.class, args);
+    }
+}
---
+++ src/test/java/com/github/pedrobacchini/enablehttpselasticbeanstalk/EnablehttpselasticbeanstalkApplicationTests.java
@@ -0,0 +1,16 @@
+package com.github.pedrobacchini.enablehttpselasticbeanstalk;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EnablehttpselasticbeanstalkApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
